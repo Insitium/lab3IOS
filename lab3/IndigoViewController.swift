f47d3b6aba809aad4ebc519a937a246395c20394
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-11-06 11:55:43 -0500

    lab 3 navigation for two pages done from root

---
+++ lab3/IndigoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IndigoViewController.swift
+//  lab3
+//
+//  Created by Sarthak Vashistha on 2022-11-06.
+//
+
+import UIKit
+
+class IndigoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Indigo Screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by Sarthak Vashistha on 2022-11-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
